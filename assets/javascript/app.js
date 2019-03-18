e51e5e69ae207d9d53c5792c33214db616428fab
Tyler Ward <T.ward26@gmail.com>
2019-03-18 17:56:20 -0500

    Added placeholder images to intermissions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,41 +6,42 @@ $(document).ready(function () {
     var unansCount = 0;
     questionCount = 0;
     var questionList = [];
-    var questionDisplay = $("<div>");
-    questionDisplay.addClass("qHeader qChoice");
     var question = $("#question");
     var choice1 = $("#choice-1");
     var choice2 = $("#choice-2");
     var choice3 = $("#choice-3");
     var choice4 = $("#choice-4");
-    var time = 30;
+    var time;
     var timerSelect = $("#timer");
     var intervalId;
     var startVisible = true;
     var attach;
-    var timeout;
 
-    questionList.push(objConstructor("What is 1+1?", ["one", "two", "three", "four"], "two"));
-    questionList.push(objConstructor("What is the third letter of the alphabet?", ["A", "B", "C", "D"], "C"));
-    questionList.push(objConstructor("What color is the sky", ["blue", "potato", "green", "yes"], "blue"));
-    questionList.push(objConstructor("What is my name?", ["Frank", "Beans", "Hugo", "Tyler"], "Tyler"));
+    //push objects into array using objConstructor function
+
+    questionList.push(objConstructor("What is 1+1?", ["one", "two", "three", "four"], "two", "https://via.placeholder.com/150"));
+    questionList.push(objConstructor("What is the third letter of the alphabet?", ["A", "B", "C", "D"], "C", "https://via.placeholder.com/150"));
+    questionList.push(objConstructor("What color is the sky", ["blue", "potato", "green", "yes"], "blue", "https://via.placeholder.com/150"));
+    questionList.push(objConstructor("What is my name?", ["Frank", "Beans", "Hugo", "Tyler"], "Tyler", "https://via.placeholder.com/150"));
+
 
     //function definitions
-    function objConstructor(question, choices, correctChoice) {
+
+    function objConstructor(question, choices, correctChoice, image) {
         return {
             question: question,
             choices: choices,
-            correctChoice: correctChoice
+            correctChoice: correctChoice,
+            image: image
         }
     }
 
     function startGame() {
-        console.log("it worked!");
         if (startVisible) {
             $("#startButton").css("display", "none");
             startVisible = false;
+            $(".hidden").css("display", "block");
         }
-        $(".hidden").css("display", "block");
         resetClock();
         question.text(questionList[questionCount].question);
         choice1.text(questionList[questionCount].choices[0]);
@@ -125,20 +126,23 @@ $(document).ready(function () {
         if (response === "correct") {
             clearInterval(intervalId);
             clearQuestion();
-            question.html("Correct!");
-            timeout = setTimeout(changeQuestion, 5000);
+            question.html("<p>Correct!</p>");
+            question.append(`<img src="${questionList[questionCount].image}"/>`)
+            setTimeout(changeQuestion, 5000);
         }
         else if (response === "incorrect") {
             clearInterval(intervalId);
             clearQuestion();
-            question.html(`Nope!<p>The Correct Answer was: ${questionList[questionCount].correctChoice}</p>`);
-            timeout = setTimeout(changeQuestion, 5000);
+            question.html(`<p>Nope!</p><p>The Correct Answer was: ${questionList[questionCount].correctChoice}</p>`);
+            question.append(`<img src="${questionList[questionCount].image}"/>`)
+            setTimeout(changeQuestion, 5000);
         }
         else if (response === "unanswered") {
             clearInterval(intervalId);
             clearQuestion();
-            question.html(`Out of Time!<p>The Correct Answer was: ${questionList[questionCount].correctChoice}</p>`)
-            timeout = setTimeout(changeQuestion, 5000);
+            question.html(`<p>Out of Time!</p><p>The Correct Answer was: ${questionList[questionCount].correctChoice}</p>`)
+            question.append(`<img src="${questionList[questionCount].image}"/>`)
+            setTimeout(changeQuestion, 5000);
         }
 
     }
@@ -152,44 +156,36 @@ $(document).ready(function () {
 
     choice1.click(function () {
         if (questionList[questionCount].choices[0] === questionList[questionCount].correctChoice) {
-            console.log("correct answer")
             correctAnswer();
         }
         else {
-            console.log("incorrect answer")
             wrongAnswer();
         }
     });
 
     choice2.click(function () {
         if (questionList[questionCount].choices[1] === questionList[questionCount].correctChoice) {
-            console.log("correct answer")
             correctAnswer();
         }
         else {
-            console.log("incorrect answer")
             wrongAnswer();
         }
     });
 
     choice3.click(function () {
         if (questionList[questionCount].choices[2] === questionList[questionCount].correctChoice) {
-            console.log("correct answer")
             correctAnswer();
         }
         else {
-            console.log("incorrect answer")
             wrongAnswer();
         }
     });
 
     choice4.click(function () {
         if (questionList[questionCount].choices[3] === questionList[questionCount].correctChoice) {
-            console.log("correct answer")
             correctAnswer();
         }
         else {
-            console.log("incorrect answer")
             wrongAnswer();
         }
     });
