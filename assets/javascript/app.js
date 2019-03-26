98867b32ff54c31c118d5c81e93714461b50617c
Tyler Ward <T.ward26@gmail.com>
2019-03-26 13:31:30 -0500

    Made questionlist hardcoded array of objects, changed onclick functionality to use only 1 event listener

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,45 +6,111 @@ $(document).ready(function () {
     var incorrCount = 0;
     var unansCount = 0;
     var questionCount = -1;
-    var questionList = [];
+    var questionList = [
+        {
+            question: "Which hyena was Whoopi Goldberg the voice of in <i>The Lion King</i>?",
+            choices: ["shenzi", "banzai", "ed", "scar"],
+            correctChoice: "shenzi", 
+            correctCap: "Shenzi",
+            image: "assets/images/hyenas.gif"
+        },
+        {
+            question: "What other name does Dr. Facilier go by in <i>The Princess and the Frog</i>?", 
+            choices: ["dr. voodoo", "mr. shadow", "shadow man", "chernabog"], 
+            correctChoice: "shadow man", 
+            correctCap: "Shadow Man", 
+            image: "assets/images/drFacilier.gif"
+        },
+        {
+            question: "In <i>Fantasia</i>, what song is associated with Chernabog?",
+            choices:  ["rite of spring", "night on bald mountain", "nutcracker suite", "sorcerer's apprentice"], 
+            correctChoice: "night on bald mountain", 
+            correctCap: "Night on Bald Mountain", 
+            image: "assets/images/chernabog.gif"
+        },
+        {
+            question: "At the end of <i>Emperor's New Groove</i>, what animal does Yzma get turned into?",
+            choices:  ["llama", "alligator", "flea", "cat"], 
+            correctChoice:"cat", 
+            correctCap: "Cat", 
+            image: "assets/images/yzma.gif"
+        },
+        {
+            question:"Who is the god of the underworld in <i>Hercules</i>?",
+            choices:  ["zeus", "apollo", "hades", "pan"], 
+            correctChoice: "hades", 
+            correctCap: "Hades", 
+            image: "assets/images/hades.gif"
+        },
+        {
+            question:"In <i>Alice in Wonderland</i>, what card suit was the Red Queen?",
+            choices:  ["hearts", "spades", "diamonds", "clubs"], 
+            correctChoice: "hearts", 
+            correctCap:"Hearts", 
+            image:"assets/images/redqueen.gif"
+        },
+        {
+            question: "What is Shere Khan afraid of in <i>The Jungle Book</i>?",
+            choices: ["humans", "bears", "thunder", "fire"], 
+            correctChoice:"fire", 
+            correctCap:"Fire", 
+            image:"assets/images/shereKhan.gif"
+        },
+        {
+            question:"What is the name of merlin's nemesis in <i>The Sword in the Stone</i>?",
+            choices: ["madame maleficent", "madame mim", "the crone", "wort"], 
+            correctChoice:"madame mim", 
+            correctCap:"Madame Mim", 
+            image:"assets/images/madameMim.gif"
+        },
+        {
+            question:"Who is the poacher in <i>Rescuers Down Under</i>?",
+            choices:  ["marahute", "joanna", "steven j. irwin", "percival c. mcleach"], 
+            correctChoice: "percival c. mcleach", 
+            correctCap:"Percival C. McLeach", 
+            image: "assets/images/mcleach.gif"
+        },
+        {
+            question:"What is the name of Gaston's sidekick in <i>Beauty and the Beast</i>>",
+            choices:  ["olaf", "lefou", "gaston jr.", "cogsworth"], 
+            correctChoice: "lefou", 
+            correctCap:"LeFou", 
+            image: "assets/images/lefou.gif"
+        },
+        {
+            question:"What other fictional character is Professor Ratigan based off of in <i>The Great Mouse Detective</i>?",
+            choices:  ["watson", "harry potter", "moriarty", "elsa"], 
+            correctChoice:"moriarty", 
+            correctCap:"Moriarty", 
+            image: "assets/images/ratigan.gif"
+        },
+        {
+            question: "What is Jafar's relation to the Sultan in <i>Aladdin</i>?",
+            choices:  ["grand vizier", "hand of the king", "son-in-law", "best friend"], 
+            correctChoice:"grand vizier", 
+            correctCap: "Grand Vizier", 
+            image: "assets/images/jafar.gif"
+        },
+        {
+            question: "What animal is Prince John in <i>Robin Hood</i>?",
+            choices: ["fox", "wolf", "lion", "vulture"], 
+            correctChoice:"lion", 
+            correctCap:"Lion", 
+            image:"assets/images/robinHoodKing.gif"
+        },
+    ];
     var question = $("#question");
-    var choice1 = $("#choice-1");
-    var choice2 = $("#choice-2");
-    var choice3 = $("#choice-3");
-    var choice4 = $("#choice-4");
+    var choice1 = $("#choice-0");
+    var choice2 = $("#choice-1");
+    var choice3 = $("#choice-2");
+    var choice4 = $("#choice-3");
     var time;
     var timerSelect = $("#timer");
     var intervalId;
     var startVisible = true;
     var attach;
 
-    //push objects into array using objConstructor function
-
-    questionList.push(objConstructor("Which hyena was Whoopi Goldberg the voice of in <i>The Lion King</i>?", ["shenzi", "banzai", "ed", "scar"], "shenzi", "Shenzi", "assets/images/hyenas.gif"));
-    questionList.push(objConstructor("What other name does Dr. Facilier go by in <i>The Princess and the Frog</i>?", ["dr. voodoo", "mr. shadow", "shadow man", "chernabog"], "shadow man", "Shadow Man", "assets/images/drFacilier.gif"));
-    questionList.push(objConstructor("In <i>Fantasia</i>, what song is associated with Chernabog?", ["rite of spring", "night on bald mountain", "nutcracker suite", "sorcerer's apprentice"], "night on bald mountain", "Night on Bald Mountain", "assets/images/chernabog.gif"));
-    questionList.push(objConstructor("At the end of <i>Emperor's New Groove</i>, what animal does Yzma get turned into?", ["llama", "alligator", "flea", "cat"], "cat", "Cat", "assets/images/yzma.gif"));
-    questionList.push(objConstructor("Who is the god of the underworld in <i>Hercules</i>?", ["zeus", "apollo", "hades", "pan"], "hades", "Hades", "assets/images/hades.gif"));
-    questionList.push(objConstructor("In <i>Alice in Wonderland</i>, what card suit was the Red Queen?", ["hearts", "spades", "diamonds", "clubs"], "hearts", "Hearts", "assets/images/redqueen.gif"));
-    questionList.push(objConstructor("What is Shere Khan afraid of in <i>The Jungle Book</i>?", ["humans", "bears", "thunder", "fire"], "fire", "Fire", "assets/images/shereKhan.gif"));
-    questionList.push(objConstructor("What is the name of merlin's nemesis in <i>The Sword in the Stone</i>?", ["madame maleficent", "madame mim", "the crone", "wort"], "madame mim", "Madame Mim", "assets/images/madameMim.gif"));
-    questionList.push(objConstructor("Who is the poacher in <i>Rescuers Down Under</i>?", ["marahute", "joanna", "steven j. irwin", "percival c. mcleach"], "percival c. mcleach", "Percival C. McLeach", "assets/images/mcleach.gif"));
-    questionList.push(objConstructor("What is the name of Gaston's sidekick in <i>Beauty and the Beast</i>>", ["olaf", "lefou", "gaston jr.", "cogsworth"], "lefou", "LeFou", "assets/images/lefou.gif"));
-    questionList.push(objConstructor("What other fictional character is Professor Ratigan based off of in <i>The Great Mouse Detective</i>?", ["watson", "harry potter", "moriarty", "elsa"], "moriarty", "Moriarty", "assets/images/ratigan.gif"));
-    questionList.push(objConstructor("What is Jafar's relation to the Sultan in <i>Aladdin</i>?", ["grand vizier", "hand of the king", "son-in-law", "best friend"], "grand vizier", "Grand Vizier", "assets/images/jafar.gif"));
-    questionList.push(objConstructor("What animal is Prince John in <i>Robin Hood</i>?", ["fox", "wolf", "lion", "vulture"], "lion", "Lion", "assets/images/robinHoodKing.gif"));
-
     //function definitions
-
-    function objConstructor(question, choices, correctChoice, correctCap, image) {
-        return {
-            question: question,
-            choices: choices,
-            correctChoice: correctChoice,
-            correctCap: correctCap,
-            image: image
-        }
-    }
 
     function startGame() {
         if (startVisible) {
@@ -122,7 +188,7 @@ $(document).ready(function () {
         }
     }
 
-    
+
     function intermission(response) {
         if (response === "correct") {
             clearInterval(intervalId);
@@ -139,56 +205,29 @@ $(document).ready(function () {
             question.html(`<p>Out of Time!</p><p>The Correct Answer was: ${questionList[questionCount].correctCap}</p>`)
             clearQuestion();
         }
-        
+
     }
-    
+
     function clearQuestion() {
         $(".hiddenQuestion").css("display", "none");
         question.append(`<img class="img-fluid imgMax" src="${questionList[questionCount].image}"/>`)
         setTimeout(changeQuestion, 5000);
     }
-    
-// on click functionality
+
+    // on click functionality
 
     $("#startButton").click(function () {
         startGame();
     });
 
-    choice1.click(function () {
-        if (questionList[questionCount].choices[0] === questionList[questionCount].correctChoice) {
+    $('.gameBtn').on('click', function() {
+        var clicked = $(this).attr("data-value");
+        if(questionList[questionCount].choices[clicked] === questionList[questionCount].correctChoice){
             correctAnswer();
         }
-        else {
+        else{
             wrongAnswer();
         }
-    });
-
-    choice2.click(function () {
-        if (questionList[questionCount].choices[1] === questionList[questionCount].correctChoice) {
-            correctAnswer();
-        }
-        else {
-            wrongAnswer();
-        }
-    });
-
-    choice3.click(function () {
-        if (questionList[questionCount].choices[2] === questionList[questionCount].correctChoice) {
-            correctAnswer();
-        }
-        else {
-            wrongAnswer();
-        }
-    });
-
-    choice4.click(function () {
-        if (questionList[questionCount].choices[3] === questionList[questionCount].correctChoice) {
-            correctAnswer();
-        }
-        else {
-            wrongAnswer();
-        }
-    });
-
-
+      });
+      
 });
